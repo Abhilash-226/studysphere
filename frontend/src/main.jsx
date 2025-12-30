fb89700d2ce650205c3c68278f547c36e18ecb80
Abhilash-226 <ugs23024_cic.abhilash@cbit.org.in>
2025-12-30 22:20:14 +0530

    Security cleanup: remove hardcoded credentials, delete backup files, update gitignore

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import "./index.css";
 
-const GOOGLE_CLIENT_ID =
-  import.meta.env.VITE_GOOGLE_CLIENT_ID ||
-  "695314240655-g5f21vnanep71gc2c2otqnc1nifdcj9v.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
